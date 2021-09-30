9e7bf7f2c6d58a8defb5e0543317bdaddf2d0186
Gustavo S. Dos Santos <simondossantos18@gmail.com>
2021-09-30 11:28:25 -0300

    Sin corregir el error de la ruta de perfil. espero que esta vez se corrija

--- resources/views/partials/navbar/menu-item-dropdown-user-menu.blade.php
+++ resources/views/partials/navbar/menu-item-dropdown-user-menu.blade.php
@@ -2,7 +2,7 @@
 @php( $profile_url = View::getSection('profile_url') ?? config('adminlte.profile_url', 'logout') )
 
 @if (config('adminlte.usermenu_profile_url', false))
-    @php( $profile_url = Auth::user()->adminlte_profile_url() )
+    @php( $profile_url = {{route('profile.show')}} )
 @endif
 
 @if (config('adminlte.use_route_url', false))
